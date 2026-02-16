23fd768fbc41a50348724c679c017d2ead361311
liger-tech <ligercybercodex@gmail.com>
2026-02-17 03:12:02 +0500

    Update Membership.jsx

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,6 +1,6 @@
 import  Memberships from "../components/sections/memberships";
 import Navbar from "../components/layout/navbar";
-import Hugetitle from "../components/layout/Hugetitle";
+
 import Footer from "../components/layout/Footer";
 
 
